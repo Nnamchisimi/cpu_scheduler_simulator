01f722657d6f75a4e91f76f08493962e55865b76
Nnamchisimi <mosimiloluwannamchi@gmail.com>
2022-12-19 17:28:42 +0200

    Added menu header

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    cout<<"*******Begining of Project********\n";
+
+    //TODO Collect command line arguement...
+    //If -f and -o is missing throw error and quit program
+    cout<<"CPU Scheduler MENU";
+    return 0;
+}
